72c635ab49052715732367f6b68ae8c18c0b1722
iuoon <710731936@qq.com>
2019-03-02 00:27:24 +0800

    修改

--- static/js/xArticleList.js
+++ static/js/xArticleList.js
@@ -1,15 +1,13 @@
 let xArtList=function () {
     var con= {
         template: `<div>
-                      <div class="card_head"><span class="span">热门文章</span></div>
-                      <div class="card">
-                        <div>
-                           <img src="http://su.semmv.com/wp-content/uploads/2016/06/2016062810005687.jpg" style="width: 100px;height: 60px;float: left">
-                        </div>
-                        <div style="margin-left: 110px;">
-                          <div><a class="link_1" @click="viewArticle(1)">我的文章</a></div>
-                          <div style="margin-top: 20px;"><span>11</span><span style="float: right;">2018-09-11</span></div>
-                        </div>
+                      <div class="cardArticle">
+                        <Card dis-hover style="border-radius: 2px;">
+                            <div>
+                                <img src="http://su.semmv.com/wp-content/uploads/2016/06/2016062810005687.jpg" width="200px" height="100px" >
+                                <span>文章文章文章文章文章文章文章文章</span>
+                            </div>
+                        </Card>
                       </div>
                    </div>`,
         data () {
